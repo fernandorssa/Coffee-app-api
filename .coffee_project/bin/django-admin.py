ea051cb772920878fa5be08c1ec3e96826cd0423
Fernando <fernando@pop-os.localdomain>
2019-11-25 16:11:03 -0300

    Second Commit

---
+++ .coffee_project/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/fernando/Dropbox/Programação/Python/Coffee_Project/Coffee-app-api/.coffee_project/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
